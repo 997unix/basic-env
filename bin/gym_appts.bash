f3c784b7c9c6d07511bc2d5c5c2ea7d320d8845f
Tony Hansmann <tony@pivotal.io>
2018-05-03 08:55:48 -0700

    Add pathing so it works with keyboard maestro

--- bin/gym_appts.bash
+++ bin/gym_appts.bash
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+PATH+="/usr/bin/:/usr/local/bin:$HOME/basic-env/bin"
+
 for i in Mon Wed Sat
- do  for j in 0 1 2 3 4
- do echo -n "$i: "
- gdate -d "$i + $j weeks" +%F
- done
- done | pcut -f 2,1 -d : -o |sort |
+ do for j in 0 1 2 3 4
+    do echo -n "$i: "
+    gdate -d "$i + $j weeks" +%F
+   done
+ done | pcut -f 2,1 -d : |sort |
  perl -ne 'chomp;
  $six="6PM";
  $ten="10AM";
