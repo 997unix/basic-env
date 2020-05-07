6f5a9b5ebfc3ed0227375b03f6a7dd969437004f
Tony Hansmann <thansmann@pivotal.io>
2020-05-07 16:23:38 -0700

    More refactoring to make a faster demo

--- workspace/2020-05-08-Tech_Friday_demo/cf_cli_demo_script_for_doitlive.sh
+++ workspace/2020-05-08-Tech_Friday_demo/cf_cli_demo_script_for_doitlive.sh
@@ -10,37 +10,34 @@ cf buildpacks #
 # //use a communinity supplied test app https://github.com/cloudfoundry-samples/test-app
 cd $HOME/workspace/test-app #
 
-# //Push a test app with no buildpack hint - demos baseline workflow
-time cf push -i 5 tech-friday-buildpack | tee ~/tmp/tech-friday-buildpack #
-export ROUTE_buildpack=$(egrep "^routes:" ~/tmp/tech-friday-buildpack | pcut) #
-echo $ROUTE_buildpack #
-
+# //Push a test app with buildpack hint - demos real-life workflow
+time cf push -b go_buildpack -i 3 tech-friday-go-buildpack #
 
 # //Push the same test app, but as a docker image from https://hub.docker.com/r/cloudfoundry/test-app
-time cf push tech-friday-docker-docker-docker -o cloudfoundry/test-app | tee ~/tmp/tech-friday-docker #
-export ROUTE_docker=$(egrep "^routes:" ~/tmp/tech-friday-buildpack | pcut) #
-echo $ROUTE_docker #
+time cf push tech-friday-docker-docker-docker -o cloudfoundry/test-app 
 
 # //scale an app
-cf scale -i 5 tech-friday-docker-docker-docker #
+time cf scale -i 2 tech-friday-docker-docker-docker #
 
 # //Show the apps running
 cf apps #
 
-# //Hit the endpoint and dump some info 
-for i in 1 2 3 4 5 ; do lynx -dump http://${ROUTE_buildpack}/ ; done #
-for i in 1 2 3 4 5 ; do lynx -dump http://${ROUTE_docker}/ ; done #
+# //tech-friday-buildpack - Hit the endpoint and dump some info 
+for i in 1 2 3 4 5 ; do lynx -dump http://tech-friday-go-buildpack.cfapps.io/ ; done | paste - - - - - - - #
+
 
 # //show some logs but limit lines
-cf logs --recent tech-friday-buildpack | tail -15
+cf logs --recent tech-friday-go-buildpack | tail -15
 
-# //Stop the app 
-cf stop tech-friday-tech-friday-docker-docker-docker #
-cf stop tech-friday-buildpack ; sleep 5 #
+# //Stop the apps 
+cf stop tech-friday-docker-docker-docker ; cf stop tech-friday-go-buildpack ; sleep 5 #
 
+# //Show the shutdown in the logs
 cf logs --recent tech-friday-buildpack | tail -15 #
 
-cf delete tech-friday-buildpack ###
-cf delete tech-friday-docker-docker-docker ###
+# //Delete the pushed apps
+cf apps| grep tech-friday| pcut -f 1 | xargs -I % cf delete -f % ###
 
+# //Clean up the route (by default, they stay after the app is deleted)
+cf routes | grep tech-fri|pcut -f 2 | xargs -I % cf delete-route cfapps.io --hostname % -f #
 
