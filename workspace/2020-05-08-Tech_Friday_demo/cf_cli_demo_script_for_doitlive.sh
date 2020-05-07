3bedec6d6a9279a30de2c76c490953bf09fcdeee
Tony Hansmann <thansmann@pivotal.io>
2020-05-07 09:17:50 -0700

    add exports to use VARs in the doitlive script

--- workspace/2020-05-08-Tech_Friday_demo/cf_cli_demo_script_for_doitlive.sh
+++ workspace/2020-05-08-Tech_Friday_demo/cf_cli_demo_script_for_doitlive.sh
@@ -5,34 +5,42 @@
 #doitlive commentecho: true
 
 # //List the resident buildpacks
-cf buildpacks
+cf buildpacks #
 
-# //reserve a couple of routes
-#
-#cf create-route development cfapps.io --hostname tech-friday-buildpack
-#cf create-route development cfapps.io --hostname tech-friday-docker-docker-docker
+# //use a communinity supplied test app https://github.com/cloudfoundry-samples/test-app
+cd $HOME/workspace/test-app #
 
-cd $HOME/workspace/test-app
-# //Push a test app with no hint to see the baseline workflow
-cf push tech-friday-buildpack --route-path tech-friday-buildpack | tee ~/tmp/tech-friday-buildpack
+# //Push a test app with no buildpack hint - demos baseline workflow
+time cf push -i 5 tech-friday-buildpack | tee ~/tmp/tech-friday-buildpack #
+export ROUTE_buildpack=$(egrep "^routes:" ~/tmp/tech-friday-buildpack | pcut) #
+echo $ROUTE_buildpack #
+
 
 # //Push the same test app, but as a docker image from https://hub.docker.com/r/cloudfoundry/test-app
-cf push tech-friday-docker-docker-docker -o cloudfoundry/test-app --route-path tech-friday-docker-docker-docker | tee ~/tmp/tech-friday-docker
+time cf push tech-friday-docker-docker-docker -o cloudfoundry/test-app | tee ~/tmp/tech-friday-docker #
+export ROUTE_docker=$(egrep "^routes:" ~/tmp/tech-friday-buildpack | pcut) #
+echo $ROUTE_docker #
 
 # //scale an app
-cf scale -i 5 tech-friday-buildpack
+cf scale -i 5 tech-friday-docker-docker-docker #
+
+# //Show the apps running
+cf apps #
 
 # //Hit the endpoint and dump some info 
-for i in 1 2 3 4 5 ; do lynx -dump http://$(egrep "^routes:" ~/tmp/tech-friday-buildpack | pcut)/ ; done
+for i in 1 2 3 4 5 ; do lynx -dump http://${ROUTE_buildpack}/ ; done #
+for i in 1 2 3 4 5 ; do lynx -dump http://${ROUTE_docker}/ ; done #
 
 # //show some logs but limit lines
 cf logs --recent tech-friday-buildpack | tail -15
 
 # //Stop the app 
-cf stop tech-friday-buildpack ; sleep 5
+cf stop tech-friday-tech-friday-docker-docker-docker #
+cf stop tech-friday-buildpack ; sleep 5 #
 
-cf logs --recent tech-friday-buildpack | tail -15
+cf logs --recent tech-friday-buildpack | tail -15 #
 
-cf delete tech-friday-buildpack 
+cf delete tech-friday-buildpack ###
+cf delete tech-friday-docker-docker-docker ###
 
 
