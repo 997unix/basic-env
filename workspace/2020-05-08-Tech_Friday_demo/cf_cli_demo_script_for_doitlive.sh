c6aaae19a3554ebef9378512da7c4f04c36db5ce
Tony Hansmann <thansmann@pivotal.io>
2020-05-07 17:40:36 -0700

    the final demoable setup - shipit

--- workspace/2020-05-08-Tech_Friday_demo/cf_cli_demo_script_for_doitlive.sh
+++ workspace/2020-05-08-Tech_Friday_demo/cf_cli_demo_script_for_doitlive.sh
@@ -17,17 +17,23 @@ cf buildpacks #
 # ---------------------------------------- 
 # //use a communinity supplied test app https://github.com/cloudfoundry-samples/test-app
 cd $HOME/workspace/test-app #
+git co 4dfe151
 
 ########################################
 # ---------------------------------------- 
+# //Look at the app manifest for the go_buildpack hint
+cat manifest.yml
+########################################
+# ---------------------------------------- 
 # //Going to push two instances of the same app: Buildpack and Docker
-# //Push a test app with buildpack hint - demos real-life workflow
-time cf push -b go_buildpack -i 3 tech-friday-go-buildpack #
+# //Push app, run 3 instances, has buildpack hint - real-life workflow
+time cf push -i 3 tech-friday-go-buildpack #
 
 ########################################
 # ---------------------------------------- 
 # //Push the same test app, but as a docker image from https://hub.docker.com/r/cloudfoundry/test-app
-time cf push tech-friday-docker-docker-docker -o cloudfoundry/test-app 
+git co a28c7ac #
+time cf push tech-friday-docker-docker-docker -o cloudfoundry/test-app #
 
 ########################################
 # ---------------------------------------- 
@@ -47,7 +53,7 @@ for i in 1 2 3 4 5 ; do lynx -dump http://tech-friday-go-buildpack.cfapps.io/ ; 
 ########################################
 # ---------------------------------------- 
 # //Stop the apps 
-cf apps | grep tech-friday| pcut -f 1 | xargs -P 3 -t -I % cf stop -f % ###
+cf apps | grep tech-friday| pcut -f 1 | xargs -P 3 -t -I % cf stop % ###
 
 ########################################
 # ---------------------------------------- 
@@ -59,3 +65,7 @@ cf apps| grep tech-friday| pcut -f 1 | xargs -P 3 -t -I % cf delete -f % ###
 # //Clean up the route (by default, they stay after the app is deleted)
 cf routes | grep tech-fri|pcut -f 2 | xargs -I % cf delete-route cfapps.io --hostname % -f #
 
+
+# ---------------------------------------- 
+# ---------------------------------------- 
+# //What are not covering: Services and Marketplace, Applications Security Groups, Isolations segments, Backup and Recovery, etc
